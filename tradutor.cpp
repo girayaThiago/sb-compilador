aec059678630c3b9ebbbe4e0237a981288c61727
girayaThiago <12597381+girayaThiago@users.noreply.github.com>
2022-08-03 17:43:59 -0300

    primeira passagem parcialmente concluida

--- tradutor.cpp
+++ tradutor.cpp
@@ -9,24 +9,30 @@
 #include "tradutor.h"
 using std::string;
 
+std::unordered_map<string, int> tabelaSimbolo;   // Tabela de simbolos;
+std::unordered_map<string, int> tabelaUso;       // Tabela de uso; // extern
+std::unordered_map<string, int> tabelaDefinicao; // Tabela de uso; // public
+std::vector<string> labelStack;
+int contador = 0;
 
-std::unordered_map<string, int>tabelaSimbolo; // Tabela de simbolos;
-std::unordered_map<string, int>tabelaUso; // Tabela de uso; // extern
-std::unordered_map<string, int>tabelaDefinicao; // Tabela de uso; // public 
-
-std::pair<Symbol, std::string> getNextToken(string& str) {
+std::pair<Symbol, std::string> getNextToken(string &str)
+{
     std::smatch m;
-    for (auto& [k, v] : patterns) {
+    for (auto &[k, v] : patterns)
+    {
 
         std::regex e(v);
 
-        while (std::regex_search(str, m, e)) {
+        while (std::regex_search(str, m, e))
+        {
             // std::cout << m[0].str() << "\n";
-            for (auto x : m) {
-                if (x.matched) {
+            for (auto x : m)
+            {
+                if (x.matched)
+                {
                     // std::cout << v << " " << x << "\n";
                     string r = x.str();
-                    std::pair<Symbol, string> kr = { k,r };
+                    std::pair<Symbol, string> kr = {k, r};
                     str = m.suffix().str();
 
                     return kr;
@@ -38,14 +44,17 @@ std::pair<Symbol, std::string> getNextToken(string& str) {
     }
     throw std::logic_error("No match during tokenization");
 }
-void tokenize(std::string str, std::vector<std::pair<Symbol, string>>& tokens) {
-    while (!str.empty()) {
+void tokenize(std::string str, std::vector<std::pair<Symbol, string>> &tokens)
+{
+    while (!str.empty())
+    {
         auto token = getNextToken(str);
         tokens.push_back(token);
     }
 }
 
-std::ostream& operator<< (std::ostream& o, Estado e) {
+std::ostream &operator<<(std::ostream &o, Estado e)
+{
     return o << statenames[e];
 }
 
@@ -55,131 +64,247 @@ std::ostream& operator<< (std::ostream& o, Estado e) {
     Start -> Seção -> Texto -> Comandos     -> Seção -> Dados -> comandos -> fim
                             -> Comandos*    -> Fim
 */
-std::unordered_map < Estado, std::function<Estado(Symbol, const string&)>> transicoes = {
-    {Estado::Inicial, [](Symbol input, const string&) {
-        switch (input) {
-            case Symbol::SECAO_TEXTO:
-            return Estado::SecaoT;
 
-            case Symbol::LABEL:
-            return Estado::TabelaSD;
+void popLabelStack()
+{
+    for (auto l : labelStack)
+        tabelaSimbolo[l] = contador;
+    labelStack.clear();
+}
+std::unordered_map<Estado, std::function<Estado(Symbol, const string &)>> transicoes = {
+    {Estado::Inicial, [](Symbol input, const string &)
+     {
+         switch (input)
+         {
+         case Symbol::SECAO_TEXTO:
+             return Estado::SecaoT;
 
-            case Symbol::SECAO_DADOS:
-            return Estado::SecaoD;
+         case Symbol::LABEL:
+             return Estado::TabelaSD;
 
-            case Symbol::PUBLIC:
-            return Estado::TabelaUso;
+         case Symbol::SECAO_DADOS:
+             return Estado::SecaoD;
 
-            default:
-            return Estado::Erro;
+         case Symbol::PUBLIC:
+             return Estado::TabelaUso;
 
-        }
-    }},
+         default:
+             return Estado::Erro;
+         }
+     }},
 
-    {Estado::SecaoT, [](Symbol input, const string& token) {
+    {Estado::SecaoT, [](Symbol input, const string &token)
+     {
+         switch (input)
+         {
+         case Symbol::INPUT:
+         case Symbol::ADD:
+         case Symbol::SUB:
+         case Symbol::MUL:
+         case Symbol::MULT:
+         case Symbol::DIV:
+         case Symbol::JMP:
+         case Symbol::JMPN:
+         case Symbol::JMPP:
+         case Symbol::JMPZ:
+         case Symbol::LOAD:
+         case Symbol::STORE:
+         case Symbol::OUTPUT:
+             contador++;
+             return Estado::Read1;
+         case Symbol::COPY:
+             contador++;
+             return Estado::Read2;
+         case Symbol::LABEL:
+             return Estado::LabelT;
+             // estado que espera estado ou um input e opera na tabela de simbolos;
+         case Symbol::STOP:
+             contador++;
+             return Estado::WaitDataOrEnd;
+         default:
+             return Estado::Erro;
+             break;
+         }
+     }},
+    {Estado::SecaoD, [](Symbol input, const string &token)
+     {
+         switch (input)
+         {
 
-        switch (input)
-        {
-        case Symbol::INPUT:
-        case Symbol::ADD:
-        case Symbol::SUB:
-        case Symbol::MUL:
-        case Symbol::MULT:
-        case Symbol::DIV:
-        case Symbol::JMP:
-        case Symbol::JMPN:
-        case Symbol::JMPP:
-        case Symbol::JMPZ:
-        case Symbol::LOAD:
-        case Symbol::STORE:
-        case Symbol::OUTPUT:
-            return Estado::Read1;
-            break;
-        case Symbol::COPY:
-            return Estado::Read2;
-            break;
-        case Symbol::LABEL:
-
-            // estado que espera estado ou um input e opera na tabela de simbolos;
-            break;
-        case Symbol::STOP:
-        default:
-            break;
-        }
-        return Estado::Erro;
-    }},
-        // {Estado::ComandosT, [](Symbol input) {
-        //     try
-        //     {
-        //         return comandos[input].e;
-        //     } catch (const std::exception& e)
-        //     {
-        //         return Estado::Erro;
-        //     }
-        //     // return input == Symbol::SECAO") || input == Symbol::FIM") ? Estado::Erro : Estado::SecaoD;
-        //     // return Estado::Inicial; // TODO: CORRIGIR ESTADO DE RETORNO>
-        //     }
-        // },
-        // {Estado::ComandosD, [](Symbol input) {
-        //     if (input == Symbol::SECAO_DADOS == 0) {
-        //         return Estado::Fim;
-        //     } else {
-        //         return Estado::ComandosD;
-        //     }
-        // }},
-        {Estado::Read2, [](Symbol input, const string& token) {
-                return input == Symbol::ID ? Estado::Read1 : Estado::Erro;
-            }
-        },
-        {Estado::Read1, [](Symbol input, const string& token) {
-            switch (input) {
-                case Symbol::ID:
-                case Symbol::NUMBER:
-                    return Estado::SecaoT;
-                default:
-                    return Estado::Erro;
-            }
-
-            }
-        },
-        {Estado::Erro, [](Symbol input, const string& token) { return Estado::Erro; }},
-        {Estado::Stop, [](Symbol input, const string& token) { return input == Symbol::SECAO_DADOS ? Estado::Erro : Estado::SecaoD; }},
-        {Estado::Fim, [](Symbol input, const string& token) { return Estado::Fim; }}
-};
+         case Symbol::LABEL:
+             labelStack.push_back(token);
+             return Estado::LabelD;
+             // estado que espera estado ou um input e opera na tabela de simbolos;
+         default:
+             return Estado::Erro;
+             break;
+         }
+     }},
+    {Estado::WaitDataOrEnd, [](Symbol input, const string &token)
+     {
+         switch (input)
+         {
+         case Symbol::SECAO_DADOS:
+             return Estado::SecaoD;
+         case Symbol::END:
+             return Estado::Fim;
+         default:
+             return Estado::Erro; // Comando ou label definido fora de seção;
+             break;
+         }
+     }},
+    // {Estado::ComandosT, [](Symbol input) {
+    //     try
+    //     {
+    //         return comandos[input].e;
+    //     } catch (const std::exception& e)
+    //     {
+    //         return Estado::Erro;
+    //     }
+    //     // return input == Symbol::SECAO") || input == Symbol::FIM") ? Estado::Erro : Estado::SecaoD;
+    //     // return Estado::Inicial; // TODO: CORRIGIR ESTADO DE RETORNO>
+    //     }
+    // },
+    // {Estado::ComandosD, [](Symbol input) {
+    //     if (input == Symbol::SECAO_DADOS == 0) {
+    //         return Estado::Fim;
+    //     } else {
+    //         return Estado::ComandosD;
+    //     }
+    // }},
+    {Estado::Read2, [](Symbol input, const string &token)
+     {
+         return input == Symbol::ID ? Estado::Read1 : Estado::Erro;
+     }},
+    {Estado::Read1, [](Symbol input, const string &token)
+     {
+         switch (input)
+         {
+         case Symbol::ID:
+         case Symbol::NUMBER:
+             return Estado::SecaoT;
+         default:
+             return Estado::Erro;
+         }
+     }},
+    {Estado::LabelT, [](Symbol input, const string &token)
+     {
+         switch (input)
+         {
+         case Symbol::INPUT:
+         case Symbol::ADD:
+         case Symbol::SUB:
+         case Symbol::MUL:
+         case Symbol::MULT:
+         case Symbol::DIV:
+         case Symbol::JMP:
+         case Symbol::JMPN:
+         case Symbol::JMPP:
+         case Symbol::JMPZ:
+         case Symbol::LOAD:
+         case Symbol::STORE:
+         case Symbol::OUTPUT:
+             for (auto l : labelStack)
+                 tabelaSimbolo[l] = contador;
+             labelStack.clear();
+             contador++;
+             return Estado::Read1;
+         case Symbol::COPY:
+             for (auto l : labelStack)
+                 tabelaSimbolo[l] = contador;
+             labelStack.clear();
+             contador++;
+             return Estado::Read2;
+         case Symbol::LABEL:
+             labelStack.push_back(token); // enquanto encontrar labels, empilhá-las pois todas apontam para a próxima referencia;
+             return Estado::LabelT;
+             // estado que espera estado ou um input e opera na tabela de simbolos;
+         case Symbol::STOP:
+             for (auto l : labelStack)
+                 tabelaSimbolo[l] = contador;
+             labelStack.clear();
+             contador++;
+         default:
+             return Estado::Erro;
+             break;
+         }
+         return Estado::Erro;
+     }},
+    {Estado::LabelD, [](Symbol input, const string &token)
+     {
+         switch (input)
+         {
+         case Symbol::LABEL:
+             labelStack.push_back(token);
+             return Estado::LabelD;
+         case Symbol::CONST:
+             // popar toda a stack de labels e fazer referencia ao endereço atual.
+             popLabelStack();
+             contador++;
+             return Estado::ReadImmediate;
+         case Symbol::SPACE:
+             popLabelStack();
+             contador++;
+             return Estado::SecaoD;
+             // tem que cuidar da tabela de definição tb;
+         default:
+             return Estado::Erro;
+             break;
+         }
+     }},
+    {Estado::ReadImmediate, [](Symbol input, const string &token)
+     {
+         switch (input)
+         {
+         case Symbol::NUMBER:
+             contador++;
+             return Estado::SecaoD;
+         default:
+             return Estado::Erro;
+         }
+     }},
+    {Estado::Erro, [](Symbol input, const string &token)
+     { return Estado::Erro; }},
+    {Estado::Stop, [](Symbol input, const string &token)
+     { return input == Symbol::SECAO_DADOS ? Estado::Erro : Estado::SecaoD; }},
+    {Estado::Fim, [](Symbol input, const string &token)
+     { return Estado::Fim; }}};
 
 // std::unordered_map<string, int>tabelaErro; // Tabela de erros;
 
-void firstPass(std::vector<std::pair<string, int>>& file) {
+void firstPass(std::vector<std::pair<string, int>> &file)
+{
     string linha;
 
     Estado estado = Estado::Inicial;
     std::vector<std::pair<Symbol, std::string>> tokens;
     // Contar as linhas, marcar ocorrências de labels com o contador;
     // ao verificar SECAO texto atualizar os valores na tabela de simbolos;
-    for (int i = 0; i < file.size(); i++) {
+    for (int i = 0; i < file.size(); i++)
+    {
         linha = file[i].first;
-        std::stringstream ss(linha);
         tokenize(linha, tokens);
     }
 
     string token;
     Symbol simbolo;
     // verificar erros;
-    for (int i = 0; i < tokens.size(); i++) {
+    for (int i = 0; i < tokens.size(); i++)
+    {
         simbolo = tokens[i].first;
         token = tokens[i].second;
         std::cout << "token: " << token << " simbolo: " << (int)simbolo << " estado atual:" << estado << " proximo estado: ";
         estado = transicoes[estado](simbolo, token);
         std::cout << estado << "\n";
-        // if (comandos.find(simbolo) != comandos.end()) {
-
-        // }
     }
+    // terminei de navegar na maquina de estados;
+    //  TODO: popular as tabelas de uso, definição e simbolos;
+    //  na teoria tamo contando corretamente os tokens de verdade, falta, navegar no código novamente, e substituir ocorrências de ID's pelos valores nas tabelas;
+    //  se um valor não existir, throw error;gi
 }
 
-
-
-std::vector<string> traduzir(std::vector<std::pair<string, int>>& processed) {
+std::vector<string> traduzir(std::vector<std::pair<string, int>> &processed)
+{
     std::vector<string> traduzido;
     int i;
     string token;
@@ -188,4 +313,3 @@ std::vector<string> traduzir(std::vector<std::pair<string, int>>& processed) {
 
     return traduzido;
 }
-
--- tradutor.h
+++ tradutor.h
@@ -8,7 +8,8 @@
 #include <stdexcept>
 #include <string>
 
-enum class Estado : int {
+enum class Estado : int
+{
     Inicial,
     SecaoT,
     ComandosT,
@@ -20,31 +21,44 @@ enum class Estado : int {
     Erro,
     Fim,
     TabelaSD,
-    TabelaUso
+    TabelaUso,
+    LabelT,
+    LabelD,
+    WaitDataOrEnd,
+    ReadImmediate
 };
 
-class Command {
+class Command
+{
     std::string OPNAME;
     int OPCODE;
     int size;
 
 public:
     Estado e;
-    Command() {
+    Command()
+    {
         throw std::logic_error("Tentando instanciar um comando sem argumentos");
     }
-    Command(std::string name, int opcode, int size) :OPNAME(name), OPCODE(opcode), size(size) {
-        if (name.compare("STOP") == 0) {
+    Command(std::string name, int opcode, int size) : OPNAME(name), OPCODE(opcode), size(size)
+    {
+        if (name.compare("STOP") == 0)
+        {
             e = Estado::Stop;
-        } else if (name.compare("COPY") == 0) {
+        }
+        else if (name.compare("COPY") == 0)
+        {
             e = Estado::Read2;
-        } else {
+        }
+        else
+        {
             e = Estado::Read1;
         }
     }
 };
 
-enum class Symbol : int {
+enum class Symbol : int
+{
     OP_BEGIN,
     LABEL,
     COMMENT,
@@ -78,70 +92,70 @@ enum class Symbol : int {
 };
 
 static std::map<Symbol, std::string> patterns = {
-        {Symbol::OP_BEGIN, "(BEGIN)"},
-        {Symbol::LABEL, "([a-zA-Z_][a-zA-Z0-9_]*[\\:])"},
-        {Symbol::COMMENT, "(;.*)"},
-        {Symbol::EQU, "(EQU )"},
-        {Symbol::IF, "(IF )"},
-        {Symbol::SECAO_TEXTO, "(SECAO TEXTO$)"},
-        {Symbol::SECAO_DADOS, "(SECAO DADOS$)"},
-        {Symbol::EXTERN, "(EXTERN )"},
-        {Symbol::PUBLIC, "(PUBLIC )"},
-        {Symbol::ADD, "(ADD )"},
-        {Symbol::SUB, "(SUB )"},
-        {Symbol::MULT, "(MULT )"},
-        {Symbol::MUL, "(MUL )"},
-        {Symbol::DIV, "(DIV )"},
-        {Symbol::JMPN, "(JMPN )"},
-        {Symbol::JMPP, "(JMPP )"},
-        {Symbol::JMPZ, "(JMPZ )"},
-        {Symbol::JMP, "(JMP )"},
-        {Symbol::COPY, "(COPY )"},
-        {Symbol::LOAD, "(LOAD )"},
-        {Symbol::STORE, "(STORE )"},
-        {Symbol::INPUT, "(INPUT )"},
-        {Symbol::OUTPUT, "(OUTPUT )"},
-        {Symbol::STOP, "(STOP$)"},
-        {Symbol::SPACE, "(SPACE$)"},
-        {Symbol::CONST, "(CONST )"},
-        {Symbol::END, "(END$)"},
-        {Symbol::ID, "([a-zA-Z_][a-zA-Z0-9_]*)"},
-        {Symbol::NUMBER, "([0-9]+)"},
-        {Symbol::ERRO, "(.)"}
-};
+    {Symbol::OP_BEGIN, "(BEGIN)"},
+    {Symbol::LABEL, "([a-zA-Z_][a-zA-Z0-9_]*[\\:])"},
+    {Symbol::COMMENT, "(;.*)"},
+    {Symbol::EQU, "(EQU )"},
+    {Symbol::IF, "(IF )"},
+    {Symbol::SECAO_TEXTO, "(SECAO TEXTO$)"},
+    {Symbol::SECAO_DADOS, "(SECAO DADOS$)"},
+    {Symbol::EXTERN, "(EXTERN )"},
+    {Symbol::PUBLIC, "(PUBLIC )"},
+    {Symbol::ADD, "(ADD )"},
+    {Symbol::SUB, "(SUB )"},
+    {Symbol::MULT, "(MULT )"},
+    {Symbol::MUL, "(MUL )"},
+    {Symbol::DIV, "(DIV )"},
+    {Symbol::JMPN, "(JMPN )"},
+    {Symbol::JMPP, "(JMPP )"},
+    {Symbol::JMPZ, "(JMPZ )"},
+    {Symbol::JMP, "(JMP )"},
+    {Symbol::COPY, "(COPY )"},
+    {Symbol::LOAD, "(LOAD )"},
+    {Symbol::STORE, "(STORE )"},
+    {Symbol::INPUT, "(INPUT )"},
+    {Symbol::OUTPUT, "(OUTPUT )"},
+    {Symbol::STOP, "(STOP$)"},
+    {Symbol::SPACE, "(SPACE$)"},
+    {Symbol::CONST, "(CONST )"},
+    {Symbol::END, "(END$)"},
+    {Symbol::ID, "([a-zA-Z_][a-zA-Z0-9_]*)"},
+    {Symbol::NUMBER, "([0-9]+)"},
+    {Symbol::ERRO, "(.)"}};
 
 static std::unordered_map<Symbol, Command> comandos = {
-    {Symbol::ADD,Command("ADD", 1, 2)},
-    {Symbol::SUB,Command("SUB", 2, 2)},
-    {Symbol::MUL,Command("MUL", 3, 2)},
-    {Symbol::MULT,Command("MUL", 3, 2)},
-    {Symbol::DIV,Command("DIV", 4, 2)},
-    {Symbol::JMP,Command("JMP", 5, 2)},
-    {Symbol::JMPN,Command("JMPN", 6, 2)},
-    {Symbol::JMPP,Command("JMPP", 7, 2)},
-    {Symbol::JMPZ,Command("JMPZ", 8, 2)},
-    {Symbol::COPY,Command("COPY", 9, 3)},
-    {Symbol::LOAD,Command("LOAD", 10, 2)},
-    {Symbol::STORE,Command("STORE", 11, 2)},
-    {Symbol::INPUT,Command("INPUT", 12, 2)},
-    {Symbol::OUTPUT,Command("OUTPUT", 13, 2)},
-    {Symbol::STOP,Command("STOP", 14, 1)}
-};
+    {Symbol::ADD, Command("ADD", 1, 2)},
+    {Symbol::SUB, Command("SUB", 2, 2)},
+    {Symbol::MUL, Command("MUL", 3, 2)},
+    {Symbol::MULT, Command("MUL", 3, 2)},
+    {Symbol::DIV, Command("DIV", 4, 2)},
+    {Symbol::JMP, Command("JMP", 5, 2)},
+    {Symbol::JMPN, Command("JMPN", 6, 2)},
+    {Symbol::JMPP, Command("JMPP", 7, 2)},
+    {Symbol::JMPZ, Command("JMPZ", 8, 2)},
+    {Symbol::COPY, Command("COPY", 9, 3)},
+    {Symbol::LOAD, Command("LOAD", 10, 2)},
+    {Symbol::STORE, Command("STORE", 11, 2)},
+    {Symbol::INPUT, Command("INPUT", 12, 2)},
+    {Symbol::OUTPUT, Command("OUTPUT", 13, 2)},
+    {Symbol::STOP, Command("STOP", 14, 1)}};
 
 static std::unordered_map<Estado, std::string> statenames = {
-    {Estado::Inicial,"Inicial"},
-    {Estado::SecaoT,"SecaoT"},
-    {Estado::ComandosT,"ComandosT"},
-    {Estado::ComandosT,"ComandosT"},
-    {Estado::Read2,"Read2"},
-    {Estado::Read1,"Read1"},
-    {Estado::Stop,"Stop"},
-    {Estado::SecaoD,"SecaoD"},
-    {Estado::ComandosD,"ComandosD"},
-    {Estado::Erro,"Erro Sintático"},
-    {Estado::Fim,"Fim"}
-};
+    {Estado::Inicial, "Inicial"},
+    {Estado::SecaoT, "SecaoT"},
+    {Estado::ComandosT, "ComandosT"},
+    {Estado::ComandosT, "ComandosT"},
+    {Estado::Read2, "Read2 (aguradando um ID "},
+    {Estado::Read1, "Read1"},
+    {Estado::WaitDataOrEnd, "WaitDataOrEnd: Fim da seção de texto, aguarda END ou SECAO_DADOS"},
+    {Estado::Stop, "Stop"},
+    {Estado::SecaoD, "SecaoD"},
+    {Estado::ComandosD, "ComandosD"},
+    {Estado::Erro, "Erro Léxico"},
+    {Estado::Fim, "Fim"},
+    {Estado::LabelT, "LabelT: Empilhando Labels SECAO_TEXTO"},
+    {Estado::LabelD, "LabelD: Empilhando Labels SECAO_DADOS"}};
 
-std::vector<std::string> traduzir(std::vector<std::pair<std::string, int>>& processed);
+std::vector<std::string> traduzir(std::vector<std::pair<std::string, int>> &processed);
 
 #endif
